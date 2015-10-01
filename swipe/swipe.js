b787a5b46f78b571c89e67bf026751923dfea58e
lijialong01 <lijialong01@baidu.com>
2015-10-01 23:59:41 +0800

    添加 swipe.js 移动端滑动事件

---
+++ swipe/swipe.js
@@ -0,0 +1,49 @@
+var swipe = function(obj,cfg){
+	var base = {
+		min_move_x: 20,
+		min_move_y: 20,
+		wipeLeft: function() {},
+		wipeRight: function() {},
+		wipeUp: function() {},
+		wipeDown: function() {},
+		preventDefaultEvents: true
+	};
+
+	for(var item in cfg){
+		base[item] = cfg[item];
+	}
+
+	var isTouchMove, startTx, startTy;
+	obj.addEventListener('touchstart', function( e ){
+		var touches = e.touches[0];
+		startTx = touches.clientX;
+		startTy = touches.clientY;
+		isTouchMove = false;
+	},false);
+	obj.addEventListener( 'touchmove', function( e ){
+		isTouchMove = true;
+		cfg.preventDefaultEvents && e.preventDefault();
+	},false);
+	obj.addEventListener('touchend', function( e ){
+		if( !isTouchMove ){return; }
+
+		var touches = e.changedTouches[0],
+		endTx = touches.clientX,
+		endTy = touches.clientY,
+		distanceX = startTx - endTx,
+		distanceY = startTy - endTy,
+		isSwipe = false;
+		if( Math.abs(distanceX) >= Math.abs(distanceY) ){
+		}
+		else{
+			if( distanceY > base.min_move_y){
+				base.wipeUp();
+			}
+			else if( distanceY < (-1) * base.min_move_y ){
+				base.wipeDown();
+			}
+		}
+
+	},false);
+};
+
